55cd2a06d03b90f1c50577cc2f31b49949e51599
Kundan Prasad Yadav <ny88119@gmail.com>
2024-06-18 11:57:18 +0530

    added user data fetching

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -6,6 +6,8 @@ const randomstring = require('randomstring');
 const nodemailer = require('nodemailer');
 const otpModel = require('../models/otp')
 
+
+
 const transporter = nodemailer.createTransport({
     service: 'gmail',
     auth: {
@@ -13,185 +15,267 @@ const transporter = nodemailer.createTransport({
         pass: 'xhni prjn chca oyfz',
     },
 });
-const sendOtp = async(req,res)=>{
-    const { email } = req.body;
-    const charset = '0123456789';
-    const otp =randomstring.generate({
-               length: 6,
-               charset: charset,
-        });
-    try{
-        const userdata = await userModel.findOne({ email });
-        if(userdata){
-        const otpExpires = new Date();
-        otpExpires.setMinutes(otpExpires.getMinutes() + 5);
-        userdata.otp=otp
-        userdata.otpExpires=otpExpires
-        await userdata.save()
+
+
+const login = async (req, res) => {
+    let email = req.body.email;
+    let pwd = req.body.password;
+    try {
+        const data = await userModel.findOne({ email });
+        if (!data) {
+            return res.status(400).json({ message: "please enter correct credentials" });
         }
-        else{
-           const newOtp = await otpModel.findOne({email})
-           const otpExpires = new Date();
-           otpExpires.setMinutes(otpExpires.getMinutes() + 5);
-           if(newOtp){
-               await otpModel.updateOne({ email: email }, { $set: { otp: otp, otpExpires: otpExpires } });
-           }else{
-            await new otpModel({
-                email:email,
-                otp:otp,
-                otpExpires:otpExpires
-             }).save()
-           }
-           
-          
+        const password = await bcrypt.compare(pwd, data.password);
+        if (!password) {
+            return res.status(400).json({ message: "please enter correct password" });
         }
-        const mailOptions = {
-            from: 'ny88119@gmail.com',
-            to: email,
-            subject: 'OTP for Registration',
-            text: `Your OTP for registration is ${otp}`,
+        const key = { user: { id: data.id, role: data.role } };
+        const options = {
+            expiresIn: '1d',
         };
-       await transporter.sendMail(mailOptions);
-       res.status(200).send({status:200});
-    }
-    catch(error){
-        console.log(error)
-        res.status(500).send({status:500});
+        const authToken = jwt.sign(key, jwtSecretKey, options);
+        return res.json({ userData: data, authToken: authToken });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
     }
 }
-//resend otp 
-const resendOtp = async(req,res)=>{
-    
-    const { email } = req.body;
-    const charset = '0123456789';
-    const otp =randomstring.generate({
-               length: 6,
-               charset: charset,
-        });
-    try{
-        const userdata = await userModel.findOne({ email });
-        if(userdata){
-        const otpExpires = new Date();
-        otpExpires.setMinutes(otpExpires.getMinutes() + 5);
-        userdata.otp=otp
-        userdata.otpExpires=otpExpires
-        await userdata.save()
-        }
-        else{
-           const newOtp = await otpModel.findOne({email})
-           const otpExpires = new Date();
-           otpExpires.setMinutes(otpExpires.getMinutes() + 5);
-           if(newOtp){
-               await otpModel.updateOne({ email: email }, { $set: { otp: otp, otpExpires: otpExpires } });
-           }else{
-             //invalid request
-             res.status(200).send({status:206});
-            
-           }
-           
-          
-        }
-        const mailOptions = {
-            from: 'ny88119@gmail.com',
-            to: email,
-            subject: 'OTP for Registration',
-            text: `Your OTP for registration is ${otp}`,
-        };
-       await transporter.sendMail(mailOptions);
-       res.status(200).send({status:200});
-    }
-    catch(error){
-        console.log(error)
-        res.status(500).send({status:500});
-    }
-}
-const register = async(req,res)=>{
-    try{
-        const { name,email,role ,password,otp} = req.body;
+
+
+
+
+
+
+
+
+const register = async (req, res) => {
+    try {
+        const { name, email, role, password, otp } = req.body;
         let otpuser = await otpModel.findOne({ email });
         console.log(otpuser)
-        if (otpuser){
+        if (otpuser) {
             if (otpuser.otpExpires < new Date()) {
                 // send response that otp expires
-                return res.status(200).json({status:202 });
+                return res.status(200).json({ status: 202 });
             }
-            if (otpuser.otp !== otp){
+            if (otpuser.otp !== otp) {
                 //send response for invalid otp
-                return res.status(200).json({status:203})
+                return res.status(200).json({ status: 203 })
             }
-            else{
-            let user = await userModel.findOne({ email }); 
-            if(!user){
-                await otpModel.findOneAndDelete({ email });
-                const salt = await bcrypt.genSalt(10);
-                const secPassword = await bcrypt.hash(password, salt);
-                const data = new userModel(
-                              {
-                                  name: name,
-                                  email: email,
-                                  password: secPassword,
-                                  role: role
-                              }
-                          );
-                         savedData=await data.save();
-                  // send response for successfull regestration
-                  res.status(200).json(savedData);
-              }
-              else {
-                  //send response already registered
-                  res.status(200).send({status:204});
-              }
+            else {
+                let user = await userModel.findOne({ email });
+                if (!user) {
+                    await otpModel.findOneAndDelete({ email });
+                    const salt = await bcrypt.genSalt(10);
+                    const secPassword = await bcrypt.hash(password, salt);
+                    const data = new userModel(
+                        {
+                            name: name,
+                            email: email,
+                            password: secPassword,
+                            role: role
+                        }
+                    );
+                    savedData = await data.save();
+                    // send response for successfull regestration
+                    res.status(200).json(savedData);
+                }
+                else {
+                    //send response already registered
+                    res.status(200).send({ status: 204 });
+                }
             }
         }
-        else{
+        else {
             //invalid request
-            res.status(200).send({status:206});
+            res.status(200).send({ status: 206 });
         }
     }
-    catch(error){
+    catch (error) {
         console.log(error)
         res.status(500).send('Server Error');
     }
 }
-const login = async (req,res)=>{
-    let email =req.body.email;
-    let pwd =req.body.password;
-    try{
-        const data = await userModel.findOne({email});
-        if (!data){
-            return res.status(400).json({message:"please enter correct credentials"});
+
+
+const sendOtp = async (req, res) => {
+    const { email } = req.body;
+    const charset = '0123456789';
+    const otp = randomstring.generate({
+        length: 6,
+        charset: charset,
+    });
+    try {
+        const userdata = await userModel.findOne({ email });
+        if (userdata) {
+            const otpExpires = new Date();
+            otpExpires.setMinutes(otpExpires.getMinutes() + 5);
+            userdata.otp = otp
+            userdata.otpExpires = otpExpires
+            await userdata.save()
         }
-        const password =await bcrypt.compare(pwd, data.password);
-        if(!password){
-            return res.status(400).json({message:"please enter correct password"});
+        else {
+            const newOtp = await otpModel.findOne({ email })
+            const otpExpires = new Date();
+            otpExpires.setMinutes(otpExpires.getMinutes() + 5);
+            if (newOtp) {
+                await otpModel.updateOne({ email: email }, { $set: { otp: otp, otpExpires: otpExpires } });
+            } else {
+                await new otpModel({
+                    email: email,
+                    otp: otp,
+                    otpExpires: otpExpires
+                }).save()
+            }
+
+
         }
-        const key = {user:{id:data.id, role:data.role} };
-        const options = {
-            expiresIn: '1d',
+        const mailOptions = {
+            from: 'ny88119@gmail.com',
+            to: email,
+            subject: 'OTP for Registration',
+            text: `Your OTP for registration is ${otp}`,
         };
-        const authToken = jwt.sign(key,jwtSecretKey,options);
-        return res.json({userData: data, authToken: authToken});
-    }catch(error){
-        res.status(500).json({message:error.message});
+        await transporter.sendMail(mailOptions);
+        res.status(200).send({ status: 200 });
+    }
+    catch (error) {
+        console.log(error)
+        res.status(500).send({ status: 500 });
     }
 }
 
-const userDetail=async(req,res)=>{
-    try{
-        console.log(req.data)
-        const id=req.data.user.id;
-        const userDetail=await userModel.findById(id);
-        console.log(userDetail)
-        res.status(200).json({status:200,userData:userDetail});
-    }catch(error){
-        console.log(error)
-        res.status(400).json({error:error});
+const verifyOtp = async (req, res) => {
+    const { email, otp } = req.body;
+    try {
+        let user = await userModel.findOne({ email });
+        if (!user) {
+            //send otp if user does not exist
+            return res.status(200).json({ status: 207 });
+        }
+        if (user.otpExpires < new Date()) {
+            // send response that otp expires
+            return res.status(200).json({ status: 202 });
+        }
+        if (user.otp !== otp) {
+            //send response for invalid otp
+            return res.status(200).json({ status: 203 })
+        }
+        user.otp = undefined;
+        user.otpExpires = undefined;
+        await user.save();
+        const key = { user: { id: user.id, role: user.role } };
+        const options = {
+            expiresIn: '1d',
+        };
+
+        const authToken = jwt.sign(key, jwtSecretKey, options);
+        return res.json({ userData: user, authToken: authToken });
+    } catch (err) {
+        console.error(err);
+        res.status(500).send('Server Error');
     }
 }
-module.exports={
+
+//resend otp 
+const resendOtp = async (req, res) => {
+
+    const { email } = req.body;
+    const charset = '0123456789';
+    const otp = randomstring.generate({
+        length: 6,
+        charset: charset,
+    });
+    try {
+        const userdata = await userModel.findOne({ email });
+        if (userdata) {
+            const otpExpires = new Date();
+            otpExpires.setMinutes(otpExpires.getMinutes() + 5);
+            userdata.otp = otp
+            userdata.otpExpires = otpExpires
+            await userdata.save()
+        }
+        else {
+            const newOtp = await otpModel.findOne({ email })
+            const otpExpires = new Date();
+            otpExpires.setMinutes(otpExpires.getMinutes() + 5);
+            if (newOtp) {
+                await otpModel.updateOne({ email: email }, { $set: { otp: otp, otpExpires: otpExpires } });
+            } else {
+                //invalid request
+                res.status(200).send({ status: 206 });
+
+            }
+
+
+        }
+        const mailOptions = {
+            from: 'ny88119@gmail.com',
+            to: email,
+            subject: 'OTP for Registration',
+            text: `Your OTP for registration is ${otp}`,
+        };
+        await transporter.sendMail(mailOptions);
+        res.status(200).send({ status: 200 });
+    }
+    catch (error) {
+        console.log(error)
+        res.status(500).send({ status: 500 });
+    }
+}
+
+
+const userDetail = async (req, res) => {
+    try {
+        console.log(req.data)
+        const id = req.data.user.id;
+        const userDetail = await userModel.findById(id);
+        console.log(userDetail)
+        res.status(200).json({ status: 200, userData: userDetail });
+    } catch (error) {
+        console.log(error)
+        res.status(400).json({ error: error });
+    }
+}
+
+const storage = multer.diskStorage({
+    destination: function (req, file, cb) {
+        // Specify the directory where the uploaded files will be stored
+        cb(null, './uploads');
+    },
+    filename: function (req, file, cb) {
+        // Specify a unique filename for uploaded files
+        cb(null, file.fieldname + '-' + Date.now() + path.extname(file.originalname));
+    }
+});
+
+router.patch('/updateuserdetails', jwt.checkJwt, uploadImage.single('profile_image'), user.updateDetails)
+
+const updateDetails = async (req, res) => {
+    try {
+        const id = req.data.user.id
+        const newProfileImage = req.file ? { profile_image: req.file.filename } : {};
+        const existingData = await userModel.findById({ _id: id });
+        if (existingData.fee_per_consultation !== req.body.fee_per_consultation
+            || existingData.work_experience !== req.body.work_experience
+            || existingData.qualification !== req.body.qualification
+            || existingData.specialization !== req.body.specialization) {
+            req.body.approval = 1;
+        }
+        const userData = await userModel.findByIdAndUpdate(id, { ...req.body, ...newProfileImage }, { new: true });
+        res.status(200).json({ data: userData });
+
+    }
+    catch (error) {
+        console.log(error)
+        res.status(404).send(error);
+    }
+}
+module.exports = {
     register,
     login,
     sendOtp,
     resendOtp,
-    userDetail
+    verifyOtp,
+    userDetail,
+    updateDetails
 }
