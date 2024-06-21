1402f89e1a504e27aed6fed1f2cc053ce7f955d6
Kundan Prasad Yadav <ny88119@gmail.com>
2024-06-21 10:48:11 +0530

    completed patientPage

--- src/components/patient/PatientPage.jsx
+++ src/components/patient/PatientPage.jsx
@@ -3,14 +3,12 @@ import PatientNavbar from './patientNavbar/PatientNavbar'
 import PatientSidebar from './patientSidebar/PatientSidebar'
 import { Outlet } from 'react-router-dom';
 import PatientRightContent from './patientRightContent/PatientRightContent';
-import Prescription from './prescription/Prescription';
 
 const PatientPage = () => {
   return (
     <div className='patient'>
       <PatientNavbar />
       <PatientSidebar />
-      <Prescription />
       <PatientRightContent>
         <Outlet />
       </PatientRightContent>
